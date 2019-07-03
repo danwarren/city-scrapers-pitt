de48d38ee7f2957e084392c002518dbcbb608bd3
Daniel A.T. Warren <dan@warrennetworkservies.com>
2019-07-03 18:26:57 -0400

    added hardcoded data

---
+++ city_scrapers/spiders/alle_sanitary.py
@@ -0,0 +1,82 @@
+from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+
+
+class AlleSanitarySpider(CityScrapersSpider):
+    name = "alle_sanitary"
+    agency = "Allegheny County Sanitary Authority (ALCOSAN)"
+    timezone = "America/New_York"
+    allowed_domains = ["www.alcosan.org"]
+    start_urls = ["http://www.alcosan.org/BoardofDirectors/2015MeetingSchedule/tabid/195/Default.aspx"]
+
+# Xcode for dates div //div[@class="Normal"]/p
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+
+        Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
+        needs.
+        """
+        for item in response.css(".meetings"):
+            meeting = Meeting(
+                title=self._parse_title(item),
+                description=self._parse_description(item),
+                classification=self._parse_classification(item),
+                start=self._parse_start(item),
+                end=self._parse_end(item),
+                all_day=self._parse_all_day(item),
+                time_notes=self._parse_time_notes(item),
+                location=self._parse_location(item),
+                links=self._parse_links(item),
+                source=self._parse_source(response),
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_title(self, item):
+        """Parse or generate meeting title."""
+        return "ALCOSAN Borad Meeting"
+
+    def _parse_description(self, item):
+        """Parse or generate meeting description."""
+        return ""
+
+    def _parse_classification(self, item):
+        """Parse or generate classification from allowed options."""
+        return NOT_CLASSIFIED
+
+    def _parse_start(self, item):
+        """Parse start datetime as a naive datetime object."""
+        return None
+
+    def _parse_end(self, item):
+        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
+        return None
+
+    def _parse_time_notes(self, item):
+        """Parse any additional notes on the timing of the meeting"""
+        return ""
+
+    def _parse_all_day(self, item):
+        """Parse or generate all-day status. Defaults to False."""
+        return False
+
+    def _parse_location(self, item):
+        """Parse or generate location."""
+        return {
+            "address": "3300 Preble Avenue, Pittsburgh, PA 15233",
+            "name": "William C. Trefz Boardroom",
+        }
+
+    def _parse_links(self, item):
+        """Parse or generate links."""
+        return [{"href": "", "title": ""}]
+
+    def _parse_source(self, response):
+        """Parse or generate source."""
+        return response.url
